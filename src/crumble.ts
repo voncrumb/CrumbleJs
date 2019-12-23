98d84bbe8dc40ede005020f3bb04a89fb520b4a1
voncrumb <sackboy129@gmail.com>
2019-12-23 20:32:24 +1000

    Added strokes

--- src/crumble.ts
+++ src/crumble.ts
@@ -134,7 +134,7 @@ function drawLineBetweenPoints(II: IImage, points: point[], T: number, C: string
         for(let i = 1; i < points.length; i++){
             let b = points[i];
             II.ctx.lineTo(b.x - thicc, b.y + thicc);
-            console.log
+
         }
 
         flipper *= -1;
@@ -199,6 +199,70 @@ function drawCurveBetweenPoints(II: IImage, points: point[], tension: number = 0
     }
 }
 
+
+function interpCurvePoints(pts: point[], tension: number = 0.5, isClosed: boolean = true, numOfSegments: number = 16){
+    let _pts = [] //clones points
+    _pts = copyPoints(pts);
+    let l = pts.length;
+    var t1x: number, t2x: number, t1y: number, t2y: number;	// tension vectors
+    let st: number, t: number, i: number;		// steps based on num. of segments
+    let c1: number, c2: number, c3: number, c4: number		// cardinal points
+    let res: point[] = [];
+
+    if(isClosed){
+        _pts.unshift(pts[l - 1]);
+        _pts.unshift(pts[l - 1]);
+       // _pts.push(pts[0]);
+        _pts.push(pts[0]);
+    }else{
+        _pts.unshift(pts[0]);
+        _pts.push(pts[l - 1]);
+    }
+
+    //loop through points
+    for(let i = 1; i < (_pts.length - 2); i++){
+        //loop through segments
+        for(let t = 0; t <= numOfSegments; t++){
+            //calc tesnsion vectors
+            t1x = (_pts[i+1].x - _pts[i-1].x) * tension;
+            t2x = (_pts[i+2].x - _pts[i].x) * tension;
+
+            t1y = (_pts[i+1].y - _pts[i-1].y) * tension;
+            t2y = (_pts[i+2].y - _pts[i].y) * tension;
+
+            // calc step
+            st = t / numOfSegments;
+
+            // calc cardinals
+            c1 =   2 * Math.pow(st, 3) 	- 3 * Math.pow(st, 2) + 1; 
+            c2 = -(2 * Math.pow(st, 3)) + 3 * Math.pow(st, 2); 
+            c3 = 	   Math.pow(st, 3)	- 2 * Math.pow(st, 2) + st; 
+            c4 = 	   Math.pow(st, 3)	- 	  Math.pow(st, 2);
+
+            // calc x and y cords with common control vectors
+            let x = c1 * _pts[i].x	+ c2 * _pts[i+1].x + c3 * t1x + c4 * t2x;
+            let y = c1 * _pts[i].y	+ c2 * _pts[i+1].y + c3 * t1y + c4 * t2y;
+
+            //store points in array
+            res.push({x: x, y: y});
+            
+        }
+    }
+    res = removeAdjacentDups(res);
+    return res;
+}
+
+function removeAdjacentDups(pts: point[]){
+    let prev: point = {x: undefined,y: undefined};
+    let res: point[] = []
+    for(let i = 0; i < pts.length; i++){
+        if(pts[i].x != prev.x && pts[i].y != prev.y){
+            res.push(pts[i]);
+        }
+        prev = pts[i];
+    }
+    return res;
+}
 
 
 function getCurvePoints(pts, tension, isClosed, numOfSegments) {
@@ -299,15 +363,24 @@ function fillPath(II: IImage, points: point[]){
  * 
  * @param II 
  * @param points 
- * @param thickness 
+ * @param thickness how thicc the line is going to be
  * @param skippyCoff 
- * @param lineCoff a number between 0-1 
+ * @param lineCoff a number between 0-1 chance to draw line on any given point
  * @param C 
+ * @param SD the Standard deviation 
  */
-function sketchyLines(II: IImage, points: point[], thickness: number, skippyCoff: number, lineCoff: number, SD: number = 6,C: any = II.color, rndOffset: number, loop: boolean = true){
-    II.ctx.strokeStyle = C;
+function sketchyLines(points: point[], skippyCoff: number, lineCoff: number, SD: number = 6, rndOffset: number, loop: boolean = true){
+
+    let _points: point[] = copyPoints(points); //copy of points as to not modify origninal array
+    let __points: point[] = copyPoints(points); //another copy
+    for(let i = 0; i < points.length; i++){
+       // __points.push(points[i]);
+    }
     
-    let numOfPoints = points.length;
+
+    let numOfPoints = _points.length;
+
+    let res: point[][] = [];
 
     for(let i = 0; i < numOfPoints; i++){
         let roll = Math.random();
@@ -319,12 +392,12 @@ function sketchyLines(II: IImage, points: point[], thickness: number, skippyCoff
                 let b = Math.round(skip());
 
                 if(b > max){
-                    //console.log(max, i, b)
+                   
                     if(loop){
                         let dif = b - max;
-                        console.log(dif)
+                     
                         for(let q = 0; q < dif; q++){
-                            points.push(points[q]);
+                            _points.push(_points[q]);
                         }
                     }else{
                         b = max;
@@ -338,12 +411,12 @@ function sketchyLines(II: IImage, points: point[], thickness: number, skippyCoff
 
                 let rndO = rnd.normal(0, rndOffset);
 
-                let p1 = points[i];
+                let p1 = _points[i];
                 let p1Offset = [rndO(), rndO()]
                 p1.x += p1Offset[0];
                 p1.y += p1Offset[1];
 
-                let p2 = points[c];
+                let p2 = _points[c];
                 let p2Offset = [rndO(), rndO()]
                 p2.x += p2Offset[0];
                 p2.y += p2Offset[1];
@@ -353,31 +426,267 @@ function sketchyLines(II: IImage, points: point[], thickness: number, skippyCoff
                 
                 for(let x = i; x < c; x++){
                     // x - i loop index 
-                    let pp = points[x];
+                    let pp = _points[x];
                     let index = x - i;
                     pp.x += (b-index)/b * p1Offset[0] + (index)/b * p2Offset[0];
                     pp.y += (b-index)/b * p1Offset[1] + (index)/b * p2Offset[1];
-                    interpPoints.push(pp);
+                    
+                    //move points closer to origin points
+                    let xx = x;
+                    if(__points[x] == undefined){
+                        xx = xx - numOfPoints;
+                        //console.log('undifined')
+                    }
+                    console.log(__points[xx], x, xx);
+                    let d = dist(pp, __points[xx]);
+                    if(d > 0){
+                        let dir = direction(pp, __points[xx]);
+                        pp.x -= d * 0.1 * dir.x;
+                        pp.y -= d * 0.1 * dir.y;
+                        interpPoints.push(pp);
+                    }
+                        
+                    
+                    
+                   
+
                     
                 }
                 interpPoints.push(p2);
                 
         
-
+                res.push(copyPoints(interpPoints));
                 //II.ctx.lineWidth = thickness;
-                drawLineBetweenPoints(II, interpPoints, thickness, C);
+                //drawLineBetweenPoints(II, interpPoints, thickness, C);
                 //drawLine(II, p1, p2, thickness, C);
             }
-            
         }
     }
+    return res;
 }
+
 
 function addToPoints(points: point[], x: number, y: number){
     for(let i = 0; i < points.length; i++){
         points[i].x += x;
         points[i].y += y;
     }
+}
+
+/**
+ * returns normilsed vector
+ * @param A 
+ */
+function vNorm(A: point){
+    let d = vDist(A);
+    let B: point = {
+        x: A.x / d,
+        y: A.y / d
+    }
+    return B;
+}
+
+
+/**
+ * returns a direction vector
+ * @param norm is vector normalised
+ */
+function direction(A: point, B: point, norm: boolean = true){
+    let C: point = {x: undefined, y: undefined};
+    C.x = A.x - B.x;
+    C.y = A.y - B.y;
+    if(norm){
+        return vNorm(C);
+    }
+    return C;
+}
+
+/**
+ * Returns distance between two points
+ * @param A 
+ * @param B 
+ * @param actual true by default. false retruns distance squared
+ */
+function dist(A: point, B: point, actual: boolean = true){
+    let dist = Math.pow(A.x - B.x, 2) + Math.pow(A.y - B.y, 2);
+    if(actual){
+        return Math.sqrt(dist);
+    }else{
+        return dist;
+    }
+    
+}
+
+/**
+ * returns the length of the vector
+ * @param A 
+ */
+function vDist(A: point, actual: boolean = true){
+    let a = Math.pow(A.x, 2) + Math.pow(A.y, 2);
+    if(actual){
+        return Math.sqrt(a);
+    }
+}
+
+function copyPoints(pts: point[]){
+    let c: point[] = []
+    for(let i = 0; i < pts.length; i++){
+        let a = pts[i];
+        let x = a.x;
+        let y = a.y;
+        let b = {x: x, y: y};
+        c.push(b);
+    }
+    return c;
+}
+
+function findCenter(pts: point[]){
+    let a = pts[0];
+    let xmin = a.x, xmax = a.x, ymin = a.y, ymax = a.y;
+
+    for(let i = 0; i < pts.length; i++){
+        let a = pts[i];
+        xmin = (a.x < xmin) ? a.x : xmin;
+        xmax = (a.x > xmax) ? a.x : xmax;
+        ymin = (a.y < ymin) ? a.y : ymin;
+        ymax = (a.y > ymax) ? a.y : ymax;
+    }
+    let cx = (xmax + xmin) / 2;
+    let cy = (ymax + ymin) / 2; 
+
+    return {x: cx, y: cy};
+}
+
+function movePointsToPoint(pts: point[], p: point, scale: number){
+    let _pts: point[] = copyPoints(pts);
+
+    for(let i = 0; i < pts.length; i++){
+        let a = pts[i];
+        let dir = direction(a, p);
+        let dis = dist(a, p);
+        
+        let b = {
+            x: a.x - scale * dis * dir.x,
+            y: a.y - scale * dis * dir.y
+        }
+        _pts[i] = b;
+    }
+    return _pts;
+}
+
+function stroke(II: IImage, A: point, B: point, attack: number, release: number){
+    II.ctx.lineWidth = 1;
+    
+    let segs = 16; //constant to be changed
+
+    let pts: point[] = [];
+
+    let dir = direction(A, B);
+    let perp = {x: dir.y, y: dir.x * -1};
+    
+    //begin path 
+    pts.push({x: A.x + perp.x * attack, y: A.y + perp.y * attack});
+
+    //draw semi-circle around point A
+    pts.push({x: A.x + dir.x * attack, y: A.y + dir.y * attack});
+
+    //something
+    pts.push({x: A.x - perp.x * attack, y: A.y - perp.y * attack});
+
+
+    pts.push({x: B.x + perp.x * release, y: B.y + perp.y * release});
+
+    //draw semi-circle around point A
+    pts.push({x: B.x - dir.y * release, y: B.y - dir.y * release});
+
+    //something
+    pts.push({x: B.x - perp.x * release, y: B.y - perp.y * release});
+
+    II.ctx.fillStyle = "black";
+
+    fillPath(II, pts);
+}
+
+function strokePoints(II: IImage, pts: point[], attack: number, release:number){
+    II.ctx.lineWidth = 1;
+    let _pts: point[]= copyPoints(pts); //copy points
+    let pt: point[] = [];
+
+    let dir1 = direction(_pts[0], _pts[1]);
+    let perp1 = {x: dir1.y, y: dir1.x * -1};
+
+    let a = _pts[0];
+
+    pt.push({x: a.x + perp1.x * attack, y: a.y + perp1.y * attack});
+    
+    pt.push({x: a.x + dir1.x * attack, y: a.y + dir1.y * attack});
+
+    pt.push({x: a.x - perp1.x * attack, y: a.y - perp1.y * attack});
+
+    let sustain: number = attack;
+
+    for(let i = 1; i < _pts.length - 1; i++){
+        sustain = release + i/(_pts.length) * (attack - release);
+
+        let dir2 = direction(_pts[i], _pts[i+1]);
+        let perp2 = {x: dir2.y, y: dir2.x * -1};
+        a = _pts[i];
+        pt.push({x: a.x - perp2.x * sustain, y: a.y - perp2.y * sustain});
+    }
+
+    a = _pts[_pts.length - 1];
+    let dir3 = direction(_pts[_pts.length - 2], a);
+    let perp3 = {x: dir3.y, y: dir3.x * -1};
+    pt.push({x: a.x + perp3.x * release, y: a.y + perp3.y * release}); 
+    pt.push({x: a.x + dir3.x * release, y: a.y + dir3.y * release});
+    pt.push({x: a.x - perp3.x * release, y: a.y - perp3.y * release});
+
+    for(let i = _pts.length - 1; i > 0; i--){
+        sustain = release + i/(_pts.length) * (attack - release);
+
+        let dir2 = direction(_pts[i], _pts[i-1]);
+        let perp2 = {x: dir2.y, y: dir2.x * -1};
+        a = _pts[i];
+        pt.push({x: a.x - perp2.x * sustain, y: a.y - perp2.y * sustain});
+    }
+
+    a = _pts[0];
+    pt.push({x: a.x + perp1.x * attack, y: a.y + perp1.y * attack});
+    
+    let interpPts = interpCurvePoints(pt, 0.1, true, 16);
+    //drawLineBetweenPoints(II, interpPts, 0.1, "black", false);
+
+    fillPath(II, interpPts);
+
+}
+
+/**
+ * This is a dirty solution that should work if you don't do crazy stuff
+ * @param pts 
+ * @param orignPts 
+ */
+function splitInterp(pts: point[], orignPts: point[]){
+    let res: point[][] = [];
+    let l = pts.length;
+
+    let tempArr: point[] = [];
+    for(let i = 0; i < l; i++){
+        tempArr.push(pts[i]);
+        for(let a = 0; a < orignPts.length; a++){
+            if(orignPts[a].x == pts[i].x && orignPts[a].y == pts[i].y){
+                if(tempArr.length > 1){
+                    res.push(copyPoints(tempArr));
+                    tempArr = [];
+                    tempArr.push(pts[i]);
+                }
+            }
+        }
+        
+    }
+    if(tempArr.length > 1){
+        res.push(copyPoints(tempArr));
+    }
+    return res;
 }
 
 
@@ -395,5 +704,12 @@ export {
     ArrayToPoints,
     point,
     sketchyLines,
-    addToPoints
+    addToPoints,
+    copyPoints,
+    interpCurvePoints,
+    movePointsToPoint,
+    findCenter,
+    stroke,
+    strokePoints,
+    splitInterp
 }
--- src/server.ts
+++ src/server.ts
@@ -11,8 +11,15 @@ import {
     ArrayToPoints, 
     point,
     sketchyLines,
-    addToPoints
-} from './imager'
+    addToPoints,
+    copyPoints,
+    interpCurvePoints,
+    movePointsToPoint,
+    findCenter,
+    stroke,
+    strokePoints,
+    splitInterp
+} from './crumble'
 
 
 import {loadImage} from 'canvas';
@@ -46,7 +53,7 @@ let faceIndex: point[][] = [
         {x:100, y:100},
         {x:300, y:100},
         {x:320, y:200},
-        {x:300, y:300},
+        {x:300, y:300},//chin
         {x:200, y:400},
         {x:100, y:300},
         {x:80, y:200},
@@ -73,7 +80,7 @@ function createHead(forehead: number, cheek: number, chin: number){
     let pts2 = faceIndex[faceTypes.asuna];
 
 
-    var tension = 0.4;
+    var tension = 1;
     let a = PointsToArray(pts2);
     let curveyPoints = ArrayToPoints(getCurvePoints(a, tension, true, 16));
 
@@ -114,13 +121,13 @@ function createHead(forehead: number, cheek: number, chin: number){
     console.log(rightSide)
     addToPoints(rightSide, -2, -2)
 
-
+    //draws lighter shades
     sketchyLines(head, ArrayToPoints(cparr), 2, 10, 0.4, 2,"#feffad", 1.3);
     
-    
-    sketchyLines(head, ArrayToPoints(PointsToArray(rightSide)), 5, 10, 0.4, 2,"#8c7b51", 1.3, false);
-    addToPoints(rightSide, -2, -2)
-    sketchyLines(head, ArrayToPoints(PointsToArray(rightSide)), 5, 10, 0.4, 2,"#8c7b51", 1.3, false);
+    //draws darker shading
+    sketchyLines(head, ArrayToPoints(PointsToArray(rightSide)), 5, 5, 0.4, 2,"#8c7b51", 1.3, false);
+    addToPoints(rightSide, -2, -2) //Crude copy method
+    sketchyLines(head, ArrayToPoints(PointsToArray(rightSide)), 5, 5, 0.4, 2,"#8c7b51", 1.3, false);
 
 
     drawLineBetweenPoints(head, ArrayToPoints(cparr), 5, "black", true)
@@ -141,5 +148,94 @@ function createHead(forehead: number, cheek: number, chin: number){
 }
 
 
+function tester(){
+    let head = new IImage(500, 500);
 
-createHead(5, 5, 5);
+    //white background
+    drawRect(head, {x:0, y:0}, 500, 500, "white");
+
+
+    let pts = faceIndex[faceTypes.asuna];
+
+    let pp: point[] = [
+        {x: 0, y:0},
+        {x: 500, y:500}
+    ]
+
+    let c = interpCurvePoints(pp, 1, false, 16);
+    
+    let a = sketchyLines(c, 5, 0.9, 2, 1.3, false);
+    
+    
+    for(let i = 0; i < a.length; i++){
+        //console.log(a[i])
+        
+        //drawLineBetweenPoints(head, a[i], 1, "black", false);
+    }
+ 
+
+    var testion = 0.4;
+    let curvery = interpCurvePoints(pts, testion, true, 16);
+
+    
+    
+    
+
+    //draws the outline of the face
+    let a1 = sketchyLines(curvery, 16, 0.2, 20, 1.3, true);
+    for(let i = 0; i < a1.length; i++){
+        let a2 = a1[i];
+        for(let o = 0; o < a2.length - 1; o++){
+            let a3 = a2[o];
+            let a4 = a2[o + 1];
+            //stroke(head, a3, a4, 2, 0, 2);
+        }
+        head.ctx.fillStyle = 'black'
+        //strokePoints(head, a2, 2, 1);
+        //drawLineBetweenPoints(head, a1[i], 5, "black", false);
+        strokePoints(head, a1[i], 2, 0);
+    }
+    //drawLineBetweenPoints(head, curvery, 5, "black", true);
+    let ahh = splitInterp(curvery, pts);
+    for(let i = 0; i < ahh.length; i++){
+        //strokePoints(head, ahh[i], 1, 0);
+    }
+    
+
+
+    let ppp: point[] = [
+        {x: 50, y: 200},
+        {x: 150, y: 50},
+        {x: 300, y: 200},
+        {x: 200, y: 400}
+    ]
+    
+    
+
+
+    //fills the face with a gradient
+    let grad = head.ctx.createLinearGradient(0, 0, 170, 0);
+    grad.addColorStop(1, "#dbc695")
+    grad.addColorStop(0, "#ad945a");
+    head.ctx.fillStyle = grad;
+    
+    let center = findCenter(curvery);
+    let dab = movePointsToPoint(curvery, center, 0.006);
+
+    //fillPath(head, dab);
+
+    
+
+   
+    //stroke(head, {x: 50, y: 50}, {x: 400, y: 400}, 10, 5, 2);
+    head.ctx.fillStyle = "black"
+    //strokePoints(head, ppp, 5, 1);
+    let ahhh = interpCurvePoints(ppp, 0.4, false)
+    //strokePoints(head, ahhh, 2, 2)
+
+    head.saveImage('./Iwantdie.png')
+}
+
+tester();
+
+//createHead(5, 5, 5);
